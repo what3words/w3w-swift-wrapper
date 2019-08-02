66b9a4c4b5a83268ce125a848537efbb9a883dbf
Dave Duprey <dave@what3words.com>
2019-08-02 16:32:45 +0100

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "what3words"
+    name: "what3words",
+    products: [
+        .library(name: "what3words", targets: ["what3words"]),
+    ],
+    targets: [
+        .target(
+            name: "what3words",
+            dependencies: [],
+            path: "Sources")
+    ]
 )
