d88803ecf21956913704773aa2e1d373e6cf0af8
Dave Duprey <dave@what3words.com>
2020-11-11 15:33:37 +0000

    setting change

--- Sources/W3WSwiftApi/Voice/W3WVoiceSettings.swift
+++ Sources/W3WSwiftApi/Voice/W3WVoiceSettings.swift
@@ -13,7 +13,6 @@ extension W3WSettings {
   
   /// the VoiceAPI endpoint
   static let voiceApiUrl      = "wss://voiceapi.what3words.com/v1"
-  //static let voiceApiUrl      = "https://voiceapi.london.dev.w3w.io/v1" // DEBUG
   static let voiceSocketPath  = "/autosuggest"
   static let voiceApiVersion  = "1.0.0"
   
