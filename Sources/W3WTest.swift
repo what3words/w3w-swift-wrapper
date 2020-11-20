9eb8033c1db55c6dc6263c3ff43f938a2660f9af
Dave Duprey <dave@what3words.com>
2020-11-20 20:07:37 +0000

    add test file

---
+++ Sources/W3WTest.swift
@@ -0,0 +1,14 @@
+//
+//  W3WSettings.swift
+//  
+//
+//  Created by Dave Duprey on 29/09/2020.
+//  Copyright © 2020 What3Words. All rights reserved.
+//
+
+
+public struct W3WTest {
+  
+  static let test = 0
+  
+}
