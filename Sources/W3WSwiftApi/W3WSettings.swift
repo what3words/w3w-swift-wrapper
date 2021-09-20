ddb4c31df9aa8ecd20556cac22160ab2c8eb8cc3
Dave Duprey <dave@what3words.com>
2021-09-20 12:09:22 +0100

    updated version numbers

--- Sources/W3WSwiftApi/W3WSettings.swift
+++ Sources/W3WSwiftApi/W3WSettings.swift
@@ -10,7 +10,7 @@
 public struct W3WSettings {
   
   static let apiUrl      = "https://api.what3words.com/v3"
-  static let apiVersion  = "3.6.7"
+  static let apiVersion  = "3.7.0"
   
   static var domains     = ["what3words.com", "w3w.io"]
   
