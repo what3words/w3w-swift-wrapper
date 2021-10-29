1b881298ac8fb7c0ce5bb1dd20a74b58c590cc5d
Dave Duprey <dave@what3words.com>
2021-10-29 11:59:44 +0100

    sonar and test adjustments

--- Package.swift
+++ Package.swift
@@ -25,10 +25,11 @@ let package = Package(
           dependencies: []),
       .testTarget(
           name: "w3w-swift-apiTests",
-          dependencies: ["W3WSwiftApi"],
-          resources: [
+          dependencies: ["W3WSwiftApi"]
+          //resources: [
             //.copy("Resources/test.dat")
-          ]),
+          //]
+      ),
       .testTarget(
         name: "w3w-objc-apiTests",
         dependencies: ["W3WSwiftApi"]
