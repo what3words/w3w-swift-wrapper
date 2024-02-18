6eb08ee5f5b3c01f577618ef0b509d1fca99033e
Dave Duprey <dave@what3words.com>
2024-02-18 11:44:48 +0000

    exposed core types

---
+++ Sources/W3WSwiftApi/Types/ExportCoreTypes.swift
@@ -0,0 +1,11 @@
+//
+//  Export.swift
+//
+//
+//  Created by Dave Duprey on 07/12/2023.
+//
+//  This exports definitions of dependancies
+//
+
+// Allows including only this package and then access to core types are available
+@_exported import W3WSwiftCore
